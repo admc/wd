b6f261a9c9a48eb67bb05013b5b7d1e1e0ba7d1e
Adam Christian <adam.christian@gmail.com>
2011-04-27 15:06:05 -0700

    Better callback use

---
+++ test/test.ondemand.ie.js
@@ -0,0 +1,16 @@
+var wd = require("../lib/main")
+
+var browser = wd.createWebDriver("ondemand.saucelabs.com", 80, "username", "accessKey");
+
+browser.init({browserName:'iexplore', version:'9'}, function() {
+  browser.get("http://www.jelly.io", function() {
+    browser.exec("window.location.href", function(o) {
+      console.log(o);
+      browser.get("http://www.seleniumhq.org", function() {
+        browser.close(function() {
+          browser.quit()
+        })
+      })
+    })
+  })
+})
