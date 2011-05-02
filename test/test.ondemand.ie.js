90fe03c806e322eb2c72559c7fbfe071aab87229
AutomatedTester <dburns@mozilla.com>
2011-05-02 08:16:51 +0100

    updated the API bringing it inline with python as it has a nicer fit

--- test/test.ondemand.ie.js
+++ test/test.ondemand.ie.js
@@ -1,6 +1,6 @@
-var wd = require("../lib/main")
+var webdriver = require("../lib/main")
 
-var browser = wd.remoteWebDriver("ondemand.saucelabs.com", 80, "username", "accessKey");
+var browser = webdriver.remoteWebDriver("ondemand.saucelabs.com", 80, "username", "accessKey");
 
 browser.init({browserName:'iexplore', version:'9'}, function() {
   browser.get("http://www.jelly.io", function() {
--- test/test.ondemand.js
+++ test/test.ondemand.js
@@ -1,6 +1,6 @@
-var wd = require("../lib/main")
+var webdriver = require("../lib/main")
 
-var browser = wd.remoteWebDriver("ondemand.saucelabs.com", 80, "username", "accesskey");
+var browser = webdriver.remote("ondemand.saucelabs.com", 80, "username", "accesskey");
 browser.init(function() {
   browser.get("http://www.jelly.io", function() {
     browser.exec("window.location.href", function(o) {
