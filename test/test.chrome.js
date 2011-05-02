da2c1c51ce1879702253b467ac891e87993cb8c3
AutomatedTester <dburns@mozilla.com>
2011-05-02 15:16:51 +0800

    updated the API bringing it inline with python as it has a nicer fit

--- test/test.chrome.js
+++ test/test.chrome.js
@@ -1,6 +1,6 @@
-var wd = require("../lib/main")
+var webdriver = require("../lib/main")
 
-var browser = wd.remoteWebDriver();
+var browser = webdriver.remote();
 browser.init({browserName:"chrome"}, function() {
   browser.get("http://www.jelly.io", function() {
     browser.exec("window.location.href", function(o) {
