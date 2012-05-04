f775aa7f7bb12eb7028c7e69f4ba7686734c8ff5
Adam Christian <adam.christian@gmail.com>
2012-05-04 15:31:24 -0700

    Making tests better and more awesome

--- test/test.ondemand.js
+++ test/test.ondemand.js
@@ -1,13 +1,28 @@
-var webdriver = require("../lib/main")
+var webdriver = require('wd')
+  , assert = require('assert');
 
-var browser = webdriver.remote("ondemand.saucelabs.com", 80, "username", "access-key");
+var browser = webdriver.remote("ondemand.saucelabs.com", 80, "username", "accessKey");
+
+browser.on('status', function(info){
+  console.log('\x1b[36m%s\x1b[0m', info);
+});
+browser.on('command', function(meth, path){
+  console.log(' > \x1b[33m%s\x1b[0m: %s', meth, path);
+});
+
+browser.desiredCapabilities.tags = ["examples"];
+browser.desiredCapabilities.name = "This is an example test";
+
 browser.init(function() {
-  browser.get("http://www.jelly.io", function() {
-    browser.eval("window.location.href", function(a, o) {
-      console.log(o);
-      browser.get("http://www.seleniumhq.org", function() {
-        browser.quit(function() {
-          console.log("DONE");
+  browser.get("http://saucelabs.com/test/guinea-pig", function() {
+    browser.title(function(err, title) {
+      assert.ok(~title.indexOf('I am a page title - Sauce Labs'), 'Wrong title!');
+      browser.elementById('submit', function(err, el) {
+        browser.clickElement(el, function() {
+          browser.eval("window.location.href", function(err, title) {
+            assert.ok(~title.indexOf('#'), 'Wrong title!');
+            browser.quit()
+          })
         })
       })
     })
