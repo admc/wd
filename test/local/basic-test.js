4530f9007433af45a689f7dcf8c6a82f36b1ad0a
sebv <seb.vincent@gmail.com>
2013-01-17 17:58:36 +0800

    travis file update

--- test/local/basic-test.js
+++ test/local/basic-test.js
@@ -6,11 +6,13 @@ test = require('../common/basic-test-base').test;
 describe("wd", function() {
   describe("local", function() {
     describe("basic test", function() {
-      describe("using chrome", function() {
-        test({}, {
-          browserName: 'chrome'
+      if(!process.env.TRAVIS_JOB_ID){
+        describe("using chrome", function() {
+          test({}, {
+            browserName: 'chrome'
+          });
         });
-      });
+      }
       describe("using firefox", function() {
         test({}, {
           browserName: 'firefox'
