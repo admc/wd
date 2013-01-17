50fe8c46ab3b1275186c234f5b9d66fc87481c9d
sebv <seb.vincent@gmail.com>
2013-01-17 17:50:54 +0800

    travis file update

--- test/local/basic-test.js
+++ test/local/basic-test.js
@@ -1,17 +1,18 @@
+console.log("------ process.env:", process.env);
 var test;
 
 test = require('../common/basic-test-base').test;
 
 describe("wd", function() {
-  return describe("local", function() {
-    return describe("basic test", function() {
+  describe("local", function() {
+    describe("basic test", function() {
       describe("using chrome", function() {
-        return test({}, {
+        test({}, {
           browserName: 'chrome'
         });
       });
-      return describe("using firefox", function() {
-        return test({}, {
+      describe("using firefox", function() {
+        test({}, {
           browserName: 'firefox'
         });
       });
