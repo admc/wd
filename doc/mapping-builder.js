d1d03f365ee520dc509693873c1c815f661cb5b4
sebv <seb.vincent@gmail.com>
2013-01-24 14:07:16 +0800

    cleaned doc tool code

--- doc/mapping-builder.js
+++ doc/mapping-builder.js
@@ -1,182 +1,118 @@
-var current, fs, isFull, jsonDoc, jsonDocs, jsonWireFull, jw_k, jw_v, l, mu, resMapping, t, tag, wd_v, _, _i, _j, _k, _l, _len, _len1, _len2, _len3, _len4, _len5, _len6, _m, _n, _o, _ref;
+var isFull = process.argv[2] === "full";
 
-isFull = process.argv[2] === "full";
-
-fs = require("fs");
-
-mu = require('mu2');
-
-_ = require('underscore');
+var fs = require("fs"),
+    mu = require('mu2'),
+    _ = require('underscore');
 
 mu.root = __dirname;
 
-jsonWireFull = JSON.parse(fs.readFileSync('doc/jsonwire-full.json').toString());
+var jsonWireFull = JSON.parse(fs.readFileSync('doc/jsonwire-full.json').toString());
+var jsonDocs = [
+  JSON.parse(fs.readFileSync('tmp/webdriver-dox.json').toString()),
+  JSON.parse(fs.readFileSync('tmp/element-dox.json').toString())
+];
 
-jsonDocs = [JSON.parse(fs.readFileSync('tmp/webdriver-dox.json').toString()), JSON.parse(fs.readFileSync('tmp/element-dox.json').toString())];
+var resMapping = [];
 
-resMapping = [];
-
-for (jw_k in jsonWireFull) {
-  jw_v = jsonWireFull[jw_k];
-  current = {
-    jsonWire: {
+// main mapping
+_(jsonWireFull).each(function (jw_v, jw_k) {
+  var current = {
+    jsonWire: { 
       key: jw_k,
       method: jw_k.split(' ')[0],
       path: jw_k.split(' ')[1],
-      url: "http://code.google.com/p/selenium/wiki/JsonWireProtocol#" + (jw_k.replace(/\s/g, '_')),
+      url: "http://code.google.com/p/selenium/wiki/JsonWireProtocol#" + jw_k.replace(/\s/g, '_'),
       desc: jw_v
     },
     wd_doc: []
   };
-  for (_i = 0, _len = jsonDocs.length; _i < _len; _i++) {
-    jsonDoc = jsonDocs[_i];
-    for (_j = 0, _len1 = jsonDoc.length; _j < _len1; _j++) {
-      wd_v = jsonDoc[_j];
-      if (((function() {
-        var _k, _len2, _ref, _results;
-        _ref = wd_v.tags;
-        _results = [];
-        for (_k = 0, _len2 = _ref.length; _k < _len2; _k++) {
-          t = _ref[_k];
-          if (t.type === 'jsonWire' && t.string === jw_k) {
-            _results.push(t);
-          }
-        }
-        return _results;
-      })()).length > 0) {
-        (function() {
-          var l, order, orderTag, _k, _len2, _ref;
-          _ref = wd_v.tags;
-          for (_k = 0, _len2 = _ref.length; _k < _len2; _k++) {
-            t = _ref[_k];
-            if (t.type === 'docOrder') {
-              orderTag = t;
-            }
-          }
-          order = orderTag? parseInt(orderTag.string) : 1000000;
-          return current.wd_doc.push({
-            'desc': (function() {
-              var _l, _len3, _ref1, _results;
-              _ref1 = wd_v.description.full.split('\n');
-              _results = [];
-              for (_l = 0, _len3 = _ref1.length; _l < _len3; _l++) {
-                l = _ref1[_l];
-                if (l !== '') {
-                  _results.push({
-                    line: l
-                  });
-                }
-              }
-              return _results;
-            })(),
-            'order': order
-          });
-        })();
+  _(jsonDocs).each(function (jsonDoc) {
+    _(jsonDoc).each(function (wd_v) {
+      if( _(wd_v.tags).filter(function (t) {
+         return (t.type === 'jsonWire') && (t.string === jw_k);
+      }).length > 0){
+        var orderTag = _(wd_v.tags).filter(function (t) {
+          return t.type === 'docOrder';
+        });
+        var order = 1000000;
+        if (orderTag.length > 0){
+         order =  parseInt(orderTag[0].string, 10); 
+        }         
+        var desc = _(wd_v.description.full.split('\n')).filter(function (l) {
+          return l !== '';
+        }).map(function (l) {
+            return {line: l};
+        });
+        current.wd_doc.push({
+          'desc': desc,
+          'order': order
+        });
       }
-    }
-  }
-  current.wd_doc = _.sortBy(current.wd_doc, function(docItem) {
+    });
+  });
+  current.wd_doc = _(current.wd_doc).sortBy(function (docItem) {
     return docItem.order;
   });
-  if (current.wd_doc.length === 0) {
-    current.wd_doc0 = true;
-  }
-  if (current.wd_doc.length === 1) {
-    current.wd_doc1 = current.wd_doc;
-  }
-  if (current.wd_doc.length > 1) {
-    current.wd_docN = current.wd_doc;
-  }
-  if (isFull || current.wd_doc.length > 0) {
-    resMapping.push(current);
-  }
-}
+  current.wd_doc0 = current.wd_doc.length === 0; 
+  current.wd_doc1 = current.wd_doc.length === 1? current.wd_doc : null;
+  current.wd_docN = current.wd_doc.length > 1? current.wd_doc: null;
 
-for (_k = 0, _len2 = jsonDocs.length; _k < _len2; _k++) {
-  jsonDoc = jsonDocs[_k];
-  for (_l = 0, _len3 = jsonDoc.length; _l < _len3; _l++) {
-    wd_v = jsonDoc[_l];
-    if (((function() {
-      var _len4, _m, _ref, _results;
-      _ref = wd_v.tags;
-      _results = [];
-      for (_m = 0, _len4 = _ref.length; _m < _len4; _m++) {
-        t = _ref[_m];
-        if (t.type === 'jsonWire') {
-          _results.push(t);
-        }
-      }
-      return _results;
-    })()).length === 0) {
+  if(isFull || (current.wd_doc.length > 0)){
+    resMapping.push(current); 
+  }
+});
+
+// extra section
+_(jsonDocs).each(function (jsonDoc) {
+  _(jsonDoc).each(function (wd_v) {
+    if(_(wd_v.tags).filter(function (t) {
+       return t.type === 'jsonWire';
+    }).length === 0){
       current = {
         extra: true,
         wd_doc: []
       };
-      current.wd_doc.push({
-        'desc': (function() {
-          var _len4, _m, _ref, _results;
-          _ref = wd_v.description.full.split('\n');
-          _results = [];
-          for (_m = 0, _len4 = _ref.length; _m < _len4; _m++) {
-            l = _ref[_m];
-            if (l !== '') {
-              _results.push({
-                line: l
-              });
-            }
-          }
-          return _results;
-        })()
+      var desc = _(wd_v.description.full.split('\n')).filter(function (l) {
+        return  l !== '';
+      }).map(function (l) {
+        return {line: l};
       });
+      current.wd_doc.push({ 'desc': desc });
       current.wd_doc1 = current.wd_doc;
       resMapping.push(current);
     }
-  }
-}
+  });  
+});
 
-for (_m = 0, _len4 = jsonDocs.length; _m < _len4; _m++) {
-  jsonDoc = jsonDocs[_m];
-  for (_n = 0, _len5 = jsonDoc.length; _n < _len5; _n++) {
-    wd_v = jsonDoc[_n];
-    _ref = wd_v.tags;
-    for (_o = 0, _len6 = _ref.length; _o < _len6; _o++) {
-      t = _ref[_o];
-      if (!(t.type === 'jsonWire')) {
-        continue;
-      }
+
+// missing section, looking for errors
+_(jsonDocs).each(function (jsonDoc) {
+  _(jsonDoc).each(function (wd_v) {
+    _(_(wd_v.tags).filter(function (t) {
+       return t.type === 'jsonWire';
+    })).each(function (t) {
       tag = t.string;
-      if (jsonWireFull[tag] == null) {
+      if(!jsonWireFull[tag]){
         current = {
-          missing: {
-            key: tag
+          missing: { 
+            key:tag
           },
           wd_doc: []
         };
-        current.wd_doc.push({
-          'desc': (function() {
-            var _len7, _p, _ref1, _results;
-            _ref1 = wd_v.description.full.split('\n');
-            _results = [];
-            for (_p = 0, _len7 = _ref1.length; _p < _len7; _p++) {
-              l = _ref1[_p];
-              if (l !== '') {
-                _results.push({
-                  line: l
-                });
-              }
-            }
-            return _results;
-          })()
+        var desc = _(wd_v.description.full.split('\n')).filter(function (l) {
+          return  l !== '';
+        }).map(function (l) {
+          return {line: l};
         });
+        current.wd_doc.push({desc: desc});
         current.wd_doc1 = current.wd_doc;
         resMapping.push(current);
-      }
-    }
-  }
-}
-
-mu.compileAndRender('mapping-template.htm', {
-  mapping: resMapping
-}).on('data', function(data) {
-  return process.stdout.write(data.toString());
+      } 
+    });
+  });  
 });
+
+mu.compileAndRender( 'mapping-template.htm', {mapping: resMapping}).on('data', function (data) {
+  process.stdout.write(data.toString());
+});
+
