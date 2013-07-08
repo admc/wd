3e335c01cdc5c69bc94f6c599fd22cc41432ef16
sebv <seb.vincent@gmail.com>
2013-07-09 01:05:30 +0800

    configuration via url package

--- test/local/wd-remote-init-test.js
+++ test/local/wd-remote-init-test.js
@@ -12,12 +12,10 @@ describe("wd", function() {
         it("browser should be initialized with default parameters", function(done) {
           var browser;
           browser = wd.remote();
-          browser.options.host.should.equal('127.0.0.1');
-          browser.options.port.should.equal(4444);
-          browser.options.path.should.equal('/wd/hub/session');
-          browser.basePath.should.equal('/wd/hub');
-          should.not.exist(browser.username);
-          should.not.exist(browser.accessKey);
+          browser.configUrl.hostname.should.equal('127.0.0.1');
+          browser.configUrl.port.should.equal('4444');
+          browser.configUrl.pathname.should.equal('/wd/hub');
+          should.not.exist(browser.configUrl.auth);
           done();
         });
       });
@@ -26,12 +24,10 @@ describe("wd", function() {
           it("browser should be initialized with given parameters", function(done) {
             var browser;
             browser = wd.remote('localhost', 8888);
-            browser.options.host.should.equal('localhost');
-            browser.options.port.should.equal(8888);
-            browser.options.path.should.equal('/wd/hub/session');
-            browser.basePath.should.equal('/wd/hub');
-            should.not.exist(browser.username);
-            should.not.exist(browser.accessKey);
+            browser.configUrl.hostname.should.equal('localhost');
+            browser.configUrl.port.should.equal('8888');
+            browser.configUrl.pathname.should.equal('/wd/hub');
+            should.not.exist(browser.configUrl.auth);
             done(null);
           });
         });
@@ -39,12 +35,10 @@ describe("wd", function() {
           it("browser should be initialized with given parameters", function(done) {
             var browser;
             browser = wd.remote('localhost', 8888, 'mickey', 'mouse');
-            browser.options.host.should.equal('localhost');
-            browser.options.port.should.equal(8888);
-            browser.options.path.should.equal('/wd/hub/session');
-            browser.basePath.should.equal('/wd/hub');
-            browser.username.should.equal('mickey');
-            browser.accessKey.should.equal('mouse');
+            browser.configUrl.hostname.should.equal('localhost');
+            browser.configUrl.port.should.equal('8888');
+            browser.configUrl.pathname.should.equal('/wd/hub');
+            browser.configUrl.auth.should.equal('mickey:mouse');
             done(null);
           });
         });
@@ -55,12 +49,10 @@ describe("wd", function() {
         it("browser should be initialized with default", function(done) {
           var browser;
           browser = wd.remote({});
-          browser.options.host.should.equal('127.0.0.1');
-          browser.options.port.should.equal(4444);
-          browser.options.path.should.equal('/wd/hub/session');
-          browser.basePath.should.equal('/wd/hub');
-          should.not.exist(browser.username);
-          should.not.exist(browser.accessKey);
+          browser.configUrl.hostname.should.equal('127.0.0.1');
+          browser.configUrl.port.should.equal('4444');
+          browser.configUrl.pathname.should.equal('/wd/hub');
+          should.not.exist(browser.configUrl.auth);
           done(null);
         });
       });
@@ -71,12 +63,10 @@ describe("wd", function() {
             host: 'localhost',
             port: 8888
           });
-          browser.options.host.should.equal('localhost');
-          browser.options.port.should.equal(8888);
-          browser.options.path.should.equal('/wd/hub/session');
-          browser.basePath.should.equal('/wd/hub');
-          should.not.exist(browser.username);
-          should.not.exist(browser.accessKey);
+          browser.configUrl.hostname.should.equal('localhost');
+          browser.configUrl.port.should.equal('8888');
+          browser.configUrl.pathname.should.equal('/wd/hub');
+          should.not.exist(browser.configUrl.auth);
           done(null);
         });
       });
@@ -89,12 +79,10 @@ describe("wd", function() {
             username: 'mickey',
             accessKey: 'mouse'
           });
-          browser.options.host.should.equal('localhost');
-          browser.options.port.should.equal(8888);
-          browser.options.path.should.equal('/wd/hub/session');
-          browser.basePath.should.equal('/wd/hub');
-          browser.username.should.equal('mickey');
-          browser.accessKey.should.equal('mouse');
+          browser.configUrl.hostname.should.equal('localhost');
+          browser.configUrl.port.should.equal('8888');
+          browser.configUrl.pathname.should.equal('/wd/hub');
+          browser.configUrl.auth.should.equal('mickey:mouse');
           done(null);
         });
       });
@@ -104,12 +92,10 @@ describe("wd", function() {
           browser = wd.remote({
             path: '/taiwan'
           });
-          browser.options.host.should.equal('127.0.0.1');
-          browser.options.port.should.equal(4444);
-          browser.options.path.should.equal('/taiwan/session');
-          browser.basePath.should.equal('/taiwan');
-          should.not.exist(browser.username);
-          should.not.exist(browser.accessKey);
+          browser.configUrl.hostname.should.equal('127.0.0.1');
+          browser.configUrl.port.should.equal('4444');
+          browser.configUrl.pathname.should.equal('/taiwan');
+          should.not.exist(browser.configUrl.auth);
           done(null);
         });
       });
@@ -121,12 +107,10 @@ describe("wd", function() {
             port: 8888,
             path: '/'
           });
-          browser.options.host.should.equal('localhost');
-          browser.options.port.should.equal(8888);
-          browser.options.path.should.equal('/session');
-          browser.basePath.should.equal('/');
-          should.not.exist(browser.username);
-          should.not.exist(browser.accessKey);
+          browser.configUrl.hostname.should.equal('localhost');
+          browser.configUrl.port.should.equal('8888');
+          browser.configUrl.pathname.should.equal('/');
+          should.not.exist(browser.configUrl.auth);
           done(null);
         });
       });
@@ -140,12 +124,10 @@ describe("wd", function() {
             accessKey: 'mouse',
             path: '/asia/taiwan'
           });
-          browser.options.host.should.equal('localhost');
-          browser.options.port.should.equal(8888);
-          browser.options.path.should.equal('/asia/taiwan/session');
-          browser.basePath.should.equal('/asia/taiwan');
-          browser.username.should.equal('mickey');
-          browser.accessKey.should.equal('mouse');
+          browser.configUrl.hostname.should.equal('localhost');
+          browser.configUrl.port.should.equal('8888');
+          browser.configUrl.pathname.should.equal('/asia/taiwan');
+          browser.configUrl.auth.should.equal('mickey:mouse');
           done(null);
         });
       });
