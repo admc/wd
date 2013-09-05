344f935df0b332383bcfe5952ba4cfb0946a7edb
sebv <seb.vincent@gmail.com>
2013-09-05 14:57:26 +0800

    switched from underscore to lodash

--- test/local/basic-test.js
+++ test/local/basic-test.js
@@ -1,4 +1,4 @@
-var _ = require("underscore");
+var _ = require("lodash");
 
 var test = require('../common/basic-test-base').test,
     utils = require('../common/utils');
--- test/saucelabs/promise-test.js
+++ test/saucelabs/promise-test.js
@@ -1,4 +1,4 @@
-var _ = require('underscore');
+var _ = require('lodash');
 
 var test = require('../common/promise-test-base').test;
 
