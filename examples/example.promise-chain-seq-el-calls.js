6674b9c7958c8286e5ff2ea4f1463f6e0e6bc6f3
sebv <seb.vincent@gmail.com>
2013-09-06 13:33:27 +0800

    added element chaining method and context management

---
+++ examples/example.promise-chain-seq-el-calls.js
@@ -0,0 +1,22 @@
+var wd;
+try {
+  wd = require('wd');
+} catch( err ) {
+  wd = require('../lib/main');
+}
+
+var browser = wd.promiseRemote();
+browser._debugPromise();
+
+browser
+  .init({browserName: 'chrome'})
+  .get('http://angularjs.org/')
+  .elementById('the-basics')
+  .text().click()//.text()
+  .elementById('the-basics')
+  .text('>').click('>').text()
+  .catch(function(err) {
+    console.log(err.stack);
+  })
+  .quit()
+  .done();
