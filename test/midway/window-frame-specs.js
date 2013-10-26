35e7c0ccd8fc5d7e4f382afc86880ea9e93d6097
sebv <seb.vincent@gmail.com>
2013-10-26 15:58:52 +0800

    split up window tests

--- test/midway/window-frame-specs.js
+++ test/midway/window-frame-specs.js
@@ -1,6 +1,13 @@
+function testInfo(testDesc) {
+  return {
+    name: "midway window - frame " + testDesc ,
+    tags: ['midway']
+  };
+}
+
 var setup = require('../helpers/setup');
 
-describe('window/frame test (' + setup.testEnv + ')', function() {
+describe('window - frame api test (' + setup.testEnv + ')', function() {
 
   var browser;
   var express = new setup.Express( __dirname + '/assets' );
@@ -9,90 +16,169 @@ describe('window/frame test (' + setup.testEnv + ')', function() {
     return title.replace(/@[-\w]+/g, '').trim();
   }
 
+  var getUrlBase = function(test) {
+    return 'http://127.0.0.1:8181/test-page?partial=' +
+      encodeURIComponent(cleanTitle(test.runnable().title));
+  };
+
   before(function() {
     express.start();
-    return browser = setup.initBrowser();
+  });
+
+  beforeEach(function() {
+    return browser = setup.remote();
+  });
+
+  afterEach(function() {
+    return setup.closeBrowser();
+  });
+
+  afterEach(function() {
+    return setup.jobStatus(this.currentTest.state === 'passed');
   });
 
   after(function() {
     express.stop();
-    return setup.closeBrowser();
   });
 
-  express.partials['browser.<window methods>'] = "";
-  it('browser.<window methods>', function() {
-    // todo: split this in separate tests
-    var urlBase = 'http://127.0.0.1:8181/test-page?partial=' +
-      encodeURIComponent(cleanTitle(this.runnable().title));
-
+  express.partials['browser.windowName'] = "";
+  it('browser.windowName', function() {
     return browser
-      .get( urlBase + "&window_num=1")
+      .init(setup.desiredWithTestInfo(testInfo('#1')))
+      .get( getUrlBase(this) + "&window_num=1")
       .execute('window.name="window-1"')
-      .windowName().should.become('window-1')
-      .windowHandle().should.eventually.have.length.above(0)
-      .newWindow(urlBase + "&window_num=2", 'window-2')
-      .window('window-2')
-      .windowName().should.become('window-2')
-      .windowHandle().then(function(w2Handle) {
-        return browser
-          .newWindow(urlBase + "&window_num=3", 'window-3')
-          .window('window-3')
-          .windowName().should.become('window-3')
-          .windowHandle().should.eventually.not.equal(w2Handle)
-          .windowHandles().should.eventually.have.length(3)
-          .window(w2Handle)
-          .windowName().should.become('window-2')
-          .close()
-          .windowHandles().should.eventually.have.length(2);
-      })
-      .window('window-3')
-      .getWindowSize('window-3').then(function(size) {
-        return browser
-          .setWindowSize(size.width -20, size.height - 10, 'window-3')
-          // todo: strange to pass the window as the last parameter
-          .getWindowSize('window-3')
-            .should.eventually.be.a('object')
-              .with.property('width', size.width -20)
-              .with.property('height', size.height - 10)
-          .setWindowSize(size.width -25, size.height - 15)
-          .getWindowSize()
-            .should.eventually.be.a('object')
-              .with.property('width', size.width -25)
-              .with.property('height', size.height - 15);
-      })
-      .getWindowPosition('window-3').then(function(pos) {
-        return browser
-          .setWindowPosition(pos.x + 10, pos.y + 5, 'window-3')
-          // todo: strange to pass the window as the last parameter
-          .getWindowPosition('window-3')
-            .should.eventually.be.a('object')
-              .with.property('x', pos.x + 10)
-              .with.property('y', pos.y + 5)
+      .windowName().should.become('window-1');
+  });
 
-          .setWindowPosition(pos.x + 15, pos.y + 10)
-          // todo: strange to pass the window as the last parameter
-          .getWindowPosition()
-            .should.eventually.be.a('object')
-              .with.property('x', pos.x + 15)
-              .with.property('y', pos.y + 10);
-      })
-      .close()
-      .windowHandles().should.eventually.have.length(1)
-      .window('window-1')
-      .newWindow(urlBase + "&window_num=4")
-      .windowHandles().then(function(handles) {
-        handles.should.have.length(2);
+  express.partials['browser.windowHandle'] = "";
+  it('browser.windowHandle', function() {
+    return browser
+      .init(setup.desiredWithTestInfo(testInfo('#2')))
+      .get( getUrlBase(this) + "&window_num=1")
+      .windowHandle().should.eventually.have.length.above(0);
+  });
+
+  express.partials['browser.newWindow'] = "";
+  it('browser.newWindow', function() {
+    return browser
+      .init(setup.desiredWithTestInfo(testInfo('#3')))
+      .get( getUrlBase(this) + "&window_num=1")
+      .newWindow(getUrlBase(this) + "&window_num=2", 'window-2');
+  });
+
+  express.partials['browser.window'] = "";
+  it('browser.window', function() {
+    var that = this;
+    return browser
+      .init(setup.desiredWithTestInfo(testInfo('#4')))
+      .get( getUrlBase(this) + "&window_num=1")
+      .windowHandle().then(function(handle1) {
         return browser
-          .window(handles[1])
-          .url().should.eventually.include("window_num=4")
-          .close()
-          .windowHandles().should.eventually.have.length(1)
-          .window(handles[0]);
+          .newWindow(getUrlBase(that) + "&window_num=2", 'window-2')
+          .window('window-2')
+          .windowName().should.become('window-2')
+          .window(handle1)
+          .windowHandle().should.become(handle1);
+      })
+      ;
+  });
+
+  express.partials['browser.windowHandles'] = "";
+  it('browser.windowHandles', function() {
+    return browser
+      .init(setup.desiredWithTestInfo(testInfo('#5')))
+      .get( getUrlBase(this) + "&window_num=1")
+      .windowHandles().should.eventually.have.length(1)
+      .newWindow(getUrlBase(this) + "&window_num=2", 'window-2')
+      .windowHandles().should.eventually.have.length(2)
+      .window('window-2')
+      .close()
+      .windowHandles().should.eventually.have.length(1);
+  });
+
+  express.partials['browser.getWindowSize'] = "";
+  it('browser.getWindowSize', function() {
+    return browser
+      .init(setup.desiredWithTestInfo(testInfo('#6')))
+      .get( getUrlBase(this) + "&window_num=1")
+      .getWindowSize().then(function(size) {
+        size.width.should.exist;
+        size.height.should.exist;
+      })
+      .windowHandle().then(function(handle) {
+        return browser
+          .getWindowSize(handle).then(function(size) {
+            size.width.should.exist;
+            size.height.should.exist;
+          });
+      });
+  });
+
+  express.partials['browser.setWindowSize'] = "";
+  it('browser.setWindowSize', function() {
+    return browser
+      .init(setup.desiredWithTestInfo(testInfo('#7')))
+      .get( getUrlBase(this) + "&window_num=1")
+      .getWindowSize().then(function(size) {
+        return browser
+          .setWindowSize(size.width - 10, size.height - 5)
+          .getWindowSize().then(function(newSize) {
+            newSize.width.should.equal(size.width - 10);
+            newSize.height.should.equal(size.height - 5);
+          })
+          .windowHandle(function(handle) {
+            return browser
+              .setWindowSize(size.width - 15, size.height - 10, handle)
+              .getWindowSize().then(function(newSize) {
+                newSize.width.should.equal(size.width - 15);
+                newSize.height.should.equal(size.height - 10);
+              });
+          });
+      });
+  });
+
+  express.partials['browser.getWindowPosition'] = "";
+  it('browser.getWindowPosition', function() {
+    return browser
+      .init(setup.desiredWithTestInfo(testInfo('#8')))
+      .get( getUrlBase(this) + "&window_num=1")
+      .getWindowPosition().then(function(pos) {
+        pos.x.should.exist;
+        pos.y.should.exist;
+      })
+      .windowHandle().then(function(handle) {
+        return browser
+          .getWindowPosition(handle).then(function(pos) {
+            pos.x.should.exist;
+            pos.y.should.exist;
+          });
+      });
+  });
+
+  express.partials['browser.setWindowPosition'] = "";
+  it('browser.setWindowPosition', function() {
+    return browser
+      .init(setup.desiredWithTestInfo(testInfo('#9')))
+      .get( getUrlBase(this) + "&window_num=1")
+      .getWindowPosition().then(function(pos) {
+        return browser
+          // not working whithout handle
+          .windowHandle(function(handle) {
+            return browser
+              .setWindowPosition(pos.width - 15, pos.height - 10, handle)
+              .getWindowPosition().then(function(newPos) {
+                newPos.width.should.equal(pos.width - 15);
+                newPos.height.should.equal(pos.height - 10);
+              });
+          })
+        ;
+
       });
   });
 
   it('browser.frame', function() {
     return browser
+      .init(setup.desiredWithTestInfo(testInfo('#10')))
       .get("http://127.0.0.1:8181/frame-test/index.html")
       .elementsByTagName('frame').should.eventually.have.length(3)
       .frame(0)
@@ -113,4 +199,5 @@ describe('window/frame test (' + setup.testEnv + ')', function() {
         });
       });
   });
+
 });
