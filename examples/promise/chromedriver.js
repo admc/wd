988df6b4237ea91aa9caafcc26d73c01249234e5
sebv <seb.vincent@gmail.com>
2013-11-30 13:53:00 +0800

    Adding chromedriver example

---
+++ examples/promise/chromedriver.js
@@ -0,0 +1,47 @@
+require('colors');
+var chai = require("chai");
+var chaiAsPromised = require("chai-as-promised");
+chai.use(chaiAsPromised);
+chai.should();
+
+var wd;
+try {
+  wd = require('wd');
+} catch( err ) {
+  wd = require('../../lib/main');
+}
+
+// enables chai assertion chaining
+chaiAsPromised.transferPromiseness = wd.transferPromiseness;
+
+var browser = wd.promiseChainRemote('http://localhost:9515/');
+
+// optional extra logging
+//browser._debugPromise();
+browser.on('status', function(info) {
+  console.log(info.cyan);
+});
+browser.on('command', function(meth, path, data) {
+  console.log(' > ' + meth.yellow, path.grey, data || '');
+});
+
+/* jshint evil: true */
+browser
+  .status()
+  .init({browserName:'chrome'})
+  .get("http://admc.io/wd/test-pages/guinea-pig.html")
+  .title()
+    .should.become('WD Tests')
+  .elementById('i am a link')
+  .click()
+  .eval("window.location.href")
+    .should.eventually.include('guinea-pig2')
+  .back()
+  .elementByCss('#comments').type('Bonjour!')
+  .getValue().should.become('Bonjour!')
+  .fin(function() { return browser.quit(); })
+  .catch(function(err) {
+    console.log(err);
+    console.log(err.data);
+  })
+  .done();
