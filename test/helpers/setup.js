265dd000054e871b33a3413914d0b90902894e1d
sebv <seb.vincent@gmail.com>
2014-05-29 12:25:03 +0800

    removed chai-as-promised require

--- test/helpers/setup.js
+++ test/helpers/setup.js
@@ -79,7 +79,6 @@ GLOBAL.prepareJs = function(script) {
 GLOBAL.Q = GLOBAL.wd.Q;
 
 var chai = require("chai");
-var chaiAsPromised = require("chai-as-promised");
 chai.use(chaiAsPromised);
 chaiAsPromised.transferPromiseness = wd.transferPromiseness;
 GLOBAL.AssertionError = chai.AssertionError;
