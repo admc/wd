6ec50d9a93778f1854cabea1264266fb84378fbc
sebv <seb.vincent@gmail.com>
2014-06-05 10:10:58 +0800

    proxy fix

--- test/specs/http-specs.js
+++ test/specs/http-specs.js
@@ -8,15 +8,24 @@ describe("wd", function() {
       var current = wd.getHttpConfig();
       wd.configureHttp({timeout: 60000, retries: 3, 'retryDelay': 15, baseUrl: 'http://example.com/' });
       wd.getHttpConfig().should.deep.equal(
-        {timeout: 60000, retries: 3, 'retryDelay': 15, baseUrl: 'http://example.com/'});
+        {timeout: 60000, retries: 3, 'retryDelay': 15,
+          baseUrl: 'http://example.com/', proxy: undefined});
       wd.configureHttp({timeout: 'default'});
       wd.getHttpConfig().should.deep.equal(
-        {timeout: undefined, retries: 3, 'retryDelay': 15, baseUrl: 'http://example.com/'});
+        {timeout: undefined, retries: 3, 'retryDelay': 15, 
+          baseUrl: 'http://example.com/', proxy: undefined});
       wd.configureHttp({retries: 'always'});
       wd.getHttpConfig().should.deep.equal(
-        {timeout: undefined, retries: 0, 'retryDelay': 15, baseUrl: 'http://example.com/'});
+        {timeout: undefined, retries: 0, 'retryDelay': 15,
+          baseUrl: 'http://example.com/', proxy: undefined});
       wd.configureHttp({retries: 'never'});
-      wd.getHttpConfig().should.deep.equal({timeout: undefined, retries: -1, 'retryDelay': 15, baseUrl: 'http://example.com/'});
+      wd.getHttpConfig().should.deep.equal(
+        {timeout: undefined, retries: -1, 'retryDelay': 15,
+          baseUrl: 'http://example.com/', proxy: undefined});
+      wd.configureHttp({proxy: 'http://proxy.com'});
+      wd.getHttpConfig().should.deep.equal(
+        {timeout: undefined, retries: -1, 'retryDelay': 15,
+          baseUrl: 'http://example.com/', proxy: 'http://proxy.com'});
       wd.configureHttp(current);
       wd.getHttpConfig().should.deep.equal(current);
       done();
