8ed028b71d66dd2f541cfe730dbdc43fe6081524
Adam Christian <adam@buoyant.io>
2018-09-19 12:43:59 -0700

    Looks like map and filter both need _.value(), removing all others

--- test/midway/add-methods-pnc-specs.js
+++ test/midway/add-methods-pnc-specs.js
@@ -84,8 +84,8 @@ describe('add-methods - promise-no-chain' + env.ENV_DESC, function() {
   };
 
   var allExtraMethodNames = _.union(
-    _(extraAsyncMethods).keys().value(),
-    _(extraPromiseNoChainMethods).keys().value()
+    _(extraAsyncMethods).keys(),
+    _(extraPromiseNoChainMethods).keys()
   );
 
   var noExtraMethodCheck = function() {
